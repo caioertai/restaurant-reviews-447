013e4b658d98111bf4e3c7f5ad5b1ef6a7eb936d
Caio Andrade <1820441+caioertai@users.noreply.github.com>
2020-08-26 07:28:25 -0500

    scaffold restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get "/restaurants/new", to: "restaurants#new" # new
+  # post "/restaurants", to: "restaurants#create" # create
+  # get "/restaurants", to: "restaurants#index" # index
+  # get "/restaurants/:id", to: "restaurants#show" # show
+  # get "/restaurants/:id/edit", to: "restaurants#edit" # edit
+  # patch "/restaurants/:id", to: "restaurants#update" # update
+  # delete "/restaurants/:id", to: "restaurants#destroy" #destroy
+
+  resources :restaurants
 end
